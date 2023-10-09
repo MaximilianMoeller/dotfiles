a65901bee3a170b68b69874491166aa9b83e5699
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2023-10-09 11:59:38 +0200

    added vim-plug plugin manager

---
+++ private_dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,11 @@
+-- https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'SirVer/ultisnips'
+Plug 'lervag/vimtex'
+
+vim.call('plug#end')
+
+
