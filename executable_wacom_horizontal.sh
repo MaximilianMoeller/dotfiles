8aa4645f454c667aa9f56c6592a6d462d29c9d03
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2023-10-16 21:31:08 +0200

    all gruvbox, added kitty config

---
+++ executable_wacom_horizontal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xsetwacom set "Wacom Intuos PT M 2 Pen stylus" Rotate half
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" touch on
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" Rotate half
+
+xsetwacom set "Wacom Intuos PT M 2 Pen stylus" MapToOutput DP-2
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" MapToOutput DP-2
---
+++ executable_wacom_vertical.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xsetwacom set "Wacom Intuos PT M 2 Pen stylus" Rotate ccw
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" touch on
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" Rotate ccw
+
+xsetwacom set "Wacom Intuos PT M 2 Pen stylus" MapToOutput DP-1
+xsetwacom set "Wacom Intuos PT M 2 Finger touch" MapToOutput DP-1
