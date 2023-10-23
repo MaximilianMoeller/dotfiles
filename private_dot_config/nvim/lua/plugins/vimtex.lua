d596a4d63f5d9b3d78e6e2fba01fcb023715d57c
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2023-10-23 17:59:48 +0200

    vimtex settings

--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -1,22 +1,15 @@
--- return {
---    "lervag/vimtex",
---    config = function()
---      vim.g.vimtex_view_view_method = 'zathura'
---	  vim.g.vimtex_mappings_prefix ='<localleader>x'
---    end,
---  }
 return {
     "lervag/vimtex",
 	lazy = false,
     init = function()
-      vim.g.vimtex_view_method = "zathura"
-	  vim.g.vimtex_mappings_prefix = "<localleader>x"
-      vim.g.vimtex_fold_enabled = 1
-	vim.g.vimtex_quickfix_ignore_filters = {
-      'Overfull',
-      'Underfull',
-	  -- this error is just useless
-	  '/m/up',
-	}
+    	vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_mappings_prefix = "<localleader>x"
+		vim.g.vimtex_quickfix_ignore_filters = {
+      		'Overfull',
+			'Underfull',
+			-- this error is just useless
+			'/m/up',
+			'Package pgf Warning: Returning node center instead of a point on node border.',
+		}
     end,
   }
