ba513e6cc66f9678ab81e463ac9d995b825ccb8d
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2024-02-02 00:37:16 +0100

    more vimtex stuff

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,52 +1,55 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 	lazy = false,
 
-  opts = {
-    highlight = { 
+	opts = {
+		highlight = { 
 			enable = true,
 			disable = { "latex" },
 		},
-    indent = { enable = true },
-    ensure_installed = {
-		-- for neovim
-      "vim",
-      "vimdoc",
-      "lua",
-      "luadoc",
-      "luap",
-		-- tools
-      "json",
-      "yaml",
-      "toml",
-      "diff",
-      "regex",
-		-- programming
-      "bash",
-      "c",
-      "cpp",
-      "python",
-		-- writing
-      "markdown",
-      "markdown_inline",
-	  "latex",
-	  "bibtex",
-    },
-  },
+		indent = {
+			enable = true,
+			disable = { "latex" },
+		},
+		ensure_installed = {
+			-- for neovim
+			"vim",
+			"vimdoc",
+			"lua",
+			"luadoc",
+			"luap",
+			-- tools
+			"json",
+			"yaml",
+			"toml",
+			"diff",
+			"regex",
+			-- programming
+			"bash",
+			"c",
+			"cpp",
+			"python",
+			-- writing
+			"markdown",
+			"markdown_inline",
+			"latex",
+			"bibtex",
+		},
+	},
 
-  config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      ---@type table<string, boolean>
-      local added = {}
-      opts.ensure_installed = vim.tbl_filter(function(lang)
-        if added[lang] then
-          return false
-        end
-        added[lang] = true
-        return true
-      end, opts.ensure_installed)
-    end
-    require("nvim-treesitter.configs").setup(opts)
-  end,
- }
+	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			---@type table<string, boolean>
+			local added = {}
+			opts.ensure_installed = vim.tbl_filter(function(lang)
+				if added[lang] then
+					return false
+				end
+				added[lang] = true
+				return true
+				end, opts.ensure_installed)
+		end
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+}
--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -5,6 +5,8 @@ return {
 		-- syntax highlighting can be done by treesitter
     	vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_mappings_prefix = "<localleader>x"
+		-- vimtex comes with its own (very basic) snippets. disable them
+		vim.g.vimtex_imaps_enabled = 0
 		vim.g.vimtex_quickfix_ignore_filters = {
       		'Overfull',
 			'Underfull',
