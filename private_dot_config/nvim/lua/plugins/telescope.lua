884a967f3de83539e157acded05603c27640a7e8
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2024-06-14 12:32:53 +0200

    added project overview to neovim $mod+y now opens neovim in project view

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,23 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	lazy = true,
+	-- load on startup so it can display recent files
+	lazy = false,
 	event = "VeryLazy",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-telescope/telescope-ui-select.nvim",
+		{
+			"ahmedkhalf/project.nvim",
+			-- the `require`-call in the `config`-function (implied by setting `opts`), needs to use 'project_nvim' instead of 'project'
+			name = "project_nvim",
+			opts = {
+				-- everything that has a `.git` subdirectory or is located in ~projects is usually a project
+				patterns = { ".git", ">~/projects" },
+				show_hidden = true,
+			}
+		}
 	},
 	opts = function()
 		return {
@@ -43,15 +54,18 @@ return {
 		telescope.setup(opts)
 		telescope.load_extension("fzf")
 		telescope.load_extension("ui-select")
+		telescope.load_extension("projects")
 	end,
 	keys = function()
 		local builtin = require("telescope.builtin")
+		local extensions = require("telescope").extensions
 		return {
-			{ "<leader>fb", builtin.buffers,     desc = "Find Buffers" },
-			{ "<leader>ff", builtin.find_files,  desc = "Find Files" },
-			{ "<leader>fg", builtin.live_grep,   desc = "Find Grep" },
-			{ "<leader>fc", builtin.colorscheme, desc = "Find Colorschemes" },
-			{ "<leader>f?", builtin.keymaps,     desc = "Vim custom normal mode keymaps" },
+			{ "<leader>fb", builtin.buffers,              desc = "Find Buffers" },
+			{ "<leader>ff", builtin.find_files,           desc = "Find Files" },
+			{ "<leader>fg", builtin.live_grep,            desc = "Find Grep" },
+			{ "<leader>fp", extensions.projects.projects, desc = "Find Projects" },
+			{ "<leader>fc", builtin.colorscheme,          desc = "Find Colorschemes" },
+			{ "<leader>f?", builtin.keymaps,              desc = "Vim custom normal mode keymaps" },
 		}
 	end,
 }
